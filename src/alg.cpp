f77afcb2187d2b79af095a010d80e7c07bd92c35
kdkdjdjd <116704003+kdkdjdjd@users.noreply.github.com>
2023-02-26 22:26:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,45 @@
 
 bool checkPrime(uint64_t value) {
 // вставьте код функции
+  for (uint64_t i = 2; i <= sqrt(value); i++) {
+    if (value % i == 0) {
+        return false;
+    }
+    return true;
+  }
 }
 
 uint64_t nPrime(uint64_t n) {
 // вставьте код функции
+  uint64_t count = 0;
+  uint64_t res = 1;
+  while (count != n) {
+    res++;
+    if (checkPrime(res) == true) {
+      count++;
+    }
+  }
+  return res;
 }
 
 uint64_t nextPrime(uint64_t value) {
 // вставьте код функции
+  if (value == 0) {
+    value++;
+  }
+  do {
+    value++;
+  } while (checkPrime(value) == false);
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
 // вставьте код функции
+  uint64_t sum = 0;
+  for (uint64_t i = 2; i < hbound; i++) {
+     if (checkPrime(i)) {
+        sum += i;
+     }
+  }
+  return sum;
 }
